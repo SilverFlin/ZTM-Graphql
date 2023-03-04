1b830f9bf053e049c33936ba5edb6cbe6de6857c
SilverFlin <toledorusso@outlook.com>
2023-03-04 15:40:04 -0700

    init commit

---
+++ products/products.model.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: "redshoe",
+    description: "Red Shoe",
+    price: 42.12,
+  },
+  {
+    id: "bluejean",
+    description: "Blue Jeans",
+    price: 55.555,
+  },
+];
+
+function getAllProducts() {
+  return products;
+}
+module.exports = {
+  getAllProducts,
+};
