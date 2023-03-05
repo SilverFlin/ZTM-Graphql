6e0e6d16f7ab7de96a6d0022e3b82b657835cc9e
SilverFlin <toledorusso@outlook.com>
2023-03-05 07:08:34 -0700

    feat: mutations add product add review

--- products/products.model.js
+++ products/products.model.js
@@ -3,13 +3,17 @@ const products = [
     id: "redshoe",
     description: "Red Shoe",
     price: 42.12,
+    reviews: [],
   },
   {
     id: "bluejean",
     description: "Blue Jeans",
     price: 55.555,
+    reviews: [],
   },
 ];
+
+const reviews = [];
 
 function getAllProducts() {
   return products;
@@ -25,8 +29,39 @@ function getProductById(id) {
   return products.find((product) => product.id === id);
 }
 
+function addNewProduct(id, description, price) {
+  const product = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+
+  products.push(product);
+  return product;
+}
+
+function addNewProductReview(id, rating, comment, productId) {
+  const product = getProductById(productId);
+  if (!product) throw Error();
+
+  const review = {
+    id,
+    rating,
+    comment,
+    product,
+  };
+
+  reviews.push(review);
+  product.reviews.push(review);
+
+  return review;
+}
+
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductById,
+  addNewProduct,
+  addNewProductReview,
 };
